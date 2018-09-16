b9e80e4d37c536c9d13e502aa7b8daa07cfa96c3
Isaac Woods <isaacwoods.home@gmail.com>
2018-09-16 01:53:14 +0100

    Move SeaBIOS DSDT data into separate hex file

--- src/aml/tests/mod.rs
+++ src/aml/tests/mod.rs
@@ -1,8 +1,9 @@
-mod seabios_dsdt;
-
+use aml::parser::AmlParser;
+use aml::stream::AmlStream;
 use log::{Log, Metadata, Record};
 use std::string::ToString;
-use {AcpiHandler, PhysicalMapping};
+use std::{collections::BTreeMap, vec::Vec};
+use {Acpi, AcpiHandler, PhysicalMapping};
 
 struct TestHandler;
 
@@ -22,6 +23,13 @@ impl AcpiHandler for TestHandler {
 
 struct TestLogger;
 
+impl TestLogger {
+    fn init() {
+        ::log::set_boxed_logger(box TestLogger).unwrap();
+        ::log::set_max_level(::log::Level::Trace.to_level_filter());
+    }
+}
+
 impl Log for TestLogger {
     fn enabled(&self, _: &Metadata) -> bool {
         true
@@ -32,4 +40,27 @@ impl Log for TestLogger {
     }
 
     fn flush(&self) {}
+}
+
+#[test]
+pub fn parse_seabios_dsdt() {
+    const DATA: &[u8] = include!("seabios_dsdt.hex");
+
+    TestLogger::init();
+
+    let mut acpi = Acpi {
+        acpi_revision: 0,
+        namespace: BTreeMap::new(),
+        boot_processor: None,
+        application_processors: Vec::new(),
+        interrupt_model: None,
+    };
+
+    let mut handler = TestHandler;
+    let stream = unsafe { AmlStream::new(DATA) };
+
+    match AmlParser::parse(&mut acpi, &mut handler, "\\", stream) {
+        Ok(_) => (),
+        Err(error) => panic!("{:?}", error),
+    }
 }
