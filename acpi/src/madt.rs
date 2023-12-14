8a065b0dc4516bc47ee8601988e94928e365c2fb
Yuuki Takano <ytakanoster@gmail.com>
2023-12-14 10:19:01 +0900

    fix: increment `processor_count` if LocalX2Apic is found
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- acpi/src/madt.rs
+++ acpi/src/madt.rs
@@ -134,6 +134,7 @@ impl Madt {
                 MadtEntry::NmiSource(_) => nmi_source_count += 1,
                 MadtEntry::LocalApicNmi(_) => local_nmi_line_count += 1,
                 MadtEntry::LocalApic(_) => processor_count += 1,
+                MadtEntry::LocalX2Apic(_) => processor_count += 1,
                 _ => (),
             }
         }
