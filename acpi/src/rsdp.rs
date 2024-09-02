152120a8e75562baac79653bbd85331158b14143
volca <volcaxiaoc@buaa.edu.cn>
2024-09-02 16:26:07 +0800

    fix the method to get from uefi system table

--- acpi/src/rsdp.rs
+++ acpi/src/rsdp.rs
@@ -1,7 +1,7 @@
-use uefi::table::{cfg::ACPI2_GUID, SystemTable};
+use uefi::table::{cfg::ACPI2_GUID, Boot, SystemTable};
 
 use crate::{AcpiError, AcpiHandler, AcpiResult, PhysicalMapping};
-use core::{mem, ops::Range, slice, str};
+use core::{mem, ops::Range, slice, str, ffi::c_void};
 
 /// The size in bytes of the ACPI 1.0 RSDP.
 const RSDP_V1_LENGTH: usize = 20;
@@ -119,12 +119,12 @@ impl Rsdp {
         let config_table = system_table.config_table();
         let rsdp = config_table.iter().find_map(|entry| {
             if entry.guid == ACPI2_GUID {
-                let rsdp_mapping = unsafe { handler.map_physical_region::<Rsdp>(entry.address, mem::size_of::<Rsdp>()) };
+                let rsdp_mapping = unsafe { handler.map_physical_region::<Rsdp>(entry.address as usize, mem::size_of::<Rsdp>()) };
                 match rsdp_mapping.validate() {
                     Ok(()) => Some(rsdp_mapping),
                     Err(AcpiError::RsdpIncorrectSignature) => None,
                     Err(err) => {
-                        log::warn!("Invalid RSDP found at {:#x}: {:?}", address, err);
+                        log::warn!("Invalid RSDP found at {:#x}: {:?}", system_table.as_ptr() as usize, err);
                         None
                     }
                 }
