b5b2dda0304c6d10daaad4c5622b589a02eddf52
Martin Hughes <martin-hughes@users.noreply.github.com>
2026-03-16 12:20:59 +0000

    Allow `aml_tester` to continue after a panic
    Fix accidentally broken tests
    
    Exit with a failure code if needed
    
    This makes it easier for any user to see if a test has failed. Which may
    be useful for example in CI environments.
    
    Remove AML filename replacement
    
    `resolve_and_compile` relies on the current `iasl` way of naming its
    output, there's no need to duplicate that in `create_script_file`

--- tests/test_infra/mod.rs
+++ tests/test_infra/mod.rs
@@ -1,13 +1,17 @@
 use acpi::Handler;
-use aml_test_tools::{new_interpreter, run_test_for_string, TestResult};
-use aml_test_tools::handlers::logging_handler::LoggingHandler;
+use aml_test_tools::{
+    RunTestResult,
+    handlers::logging_handler::LoggingHandler,
+    new_interpreter,
+    run_test_for_string,
+};
 
 pub fn run_aml_test(asl: &'static str, handler: impl Handler) {
     // Tests calling `run_aml_test` don't do much else, and we usually want logging, so initialize it here.
     let _ = pretty_env_logger::try_init();
-    
-    let logged_handler = LoggingHandler::new(handler);
-    let mut interpreter = new_interpreter(logged_handler);
 
-    assert_eq!(run_test_for_string(asl, &mut interpreter), TestResult::Pass);
+    let logged_handler = LoggingHandler::new(handler);
+    let interpreter = new_interpreter(logged_handler);
+
+    assert!(matches!(run_test_for_string(asl, interpreter), RunTestResult::Pass(_)));
 }
